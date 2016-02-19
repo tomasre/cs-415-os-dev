2c8182c8eec66389daa34a6a0f83f4db9ac03878
Tomas <me@tomasre.com>
2016-02-19 14:48:12 -0800

    initial disk files

---
+++ os/os.fs.disk.js
@@ -0,0 +1,11 @@
+'use strict';
+(function () {
+
+    /*
+    Stats.js initial file
+     */
+    os._internals.fs.disk['Stats_Data.csv'] = '1,2,3,4,5,6\n4,5,6,7,8,9\n';
+
+    // add more initial files here
+
+})();
