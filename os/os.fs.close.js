f93e191aad9553426f38871ee5c510978f0ffd6c
rliu245 <rliu245@gmail.com>
2016-02-23 00:12:44 -0800

    filled out half to 3 quarters of fs.close

--- os/os.fs.close.js
+++ os/os.fs.close.js
@@ -3,7 +3,24 @@
 (function() {
   os.fs.close = closeFile;
   
-  var closeFile = function(errorClosingFile, errorMessage) {
+  var closeFile = function(FNAME, cb) {
+    var psname = os._internals.ps.runningProcess.slice(0);
     
+    os._internals.fs.operationQueue.push({
+      operation: function(){
+        setTimeout(function(){
+          performCloseFile(psname, FNAME, cb);
+        },generateRandomTimeout());
+      },
+      processName: process
+    });
   };
+  
+  var performCloseFile = function(psname, FNAME, cb) {
+    var entrypoint;
+  };
+  
+  function generateRandomTimeout() {
+        return Math.floor(Math.random() * (100 - 10) + 10);
+    }
 )}();
