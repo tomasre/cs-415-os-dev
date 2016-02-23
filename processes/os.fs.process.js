c92199658827233ab65aeedf955c8a9a25e9de0e
Tomas <me@tomasre.com>
2016-02-23 09:06:55 -0800

    added the actual fs process

---
+++ processes/os.fs.process.js
@@ -0,0 +1,16 @@
+'use strict';
+(function () {
+
+    os.ps.register('fs', main);
+
+    /*
+    main executes one of the operations in the filesystem queue
+    note the entrypoint for this process should never change
+    main function should execute every time one wishes a filesystem operation to be performed
+     */
+    function main () {
+        var operation = os._internals.fs.operationQueue.pop();
+
+        operation.operation();
+    }
+})();
