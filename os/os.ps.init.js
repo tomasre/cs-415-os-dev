b4ae59c18b01d5dc8fbe38d95d6f9212e8b48f86
Tomas <me@tomasre.com>
2016-02-23 20:25:24 -0800

    changed logging to snapshot

--- os/os.ps.init.js
+++ os/os.ps.init.js
@@ -3,8 +3,7 @@
 
 
     console.log('OS STATE BEFORE INIT');
-    console.log(os);
-
+    console.log(JSON.parse(JSON.stringify(os)));
     // actually start the execution of the processes!
     os._internals.ps.scheduleProcess();
 })();
