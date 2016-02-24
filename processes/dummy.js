512e81cef878813ff1fc79619193b77b8a9dd327
Tomas <me@tomasre.com>
2016-02-23 19:47:44 -0800

    added logging to test

--- processes/dummy.js
+++ processes/dummy.js
@@ -5,6 +5,7 @@
 
     function main() {
         os.fs.open('EXAMPLE_FILENAME.TXT', function (errorOpen, fh) {
+            console.log('fs open success');
             if (errorOpen) {
                 // ERROR OPENING FILE, as a process I HAVE TO HANDLE THIS here!!!!!!!!
                 console.log(errorOpen);
