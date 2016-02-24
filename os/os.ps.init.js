dc0c3a0a830e2b2af83209077ae6342ff1890452
Tomas <me@tomasre.com>
2016-02-23 19:57:54 -0800

    added logging to init

--- os/os.ps.init.js
+++ os/os.ps.init.js
@@ -1,6 +1,10 @@
 'use strict';
 (function () {
 
+
+    console.log('OS STATE BEFORE INIT');
+    console.log(os);
+
     // actually start the execution of the processes!
     os._internals.ps.scheduleProcess();
 });
