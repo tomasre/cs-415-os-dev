c5f846cb2074ee1f1fd69695f6cf3ee5350d7ec0
Tomas <me@tomasre.com>
2016-03-06 16:14:27 -0800

    creates the stout and stdin streams, wrapes the miain entrypoint to pass in stdout and stdin objects when the scheduler invokes main

--- os/os.ps.registerProcess.js
+++ os/os.ps.registerProcess.js
@@ -8,12 +8,27 @@
      */
     function registerProcess(name, cb) {
 
-        os._internals.ps.pcb.push({
+        var pcb = {
             id: os._internals.ps.pcb.length,
             name: name,
             state: os._internals.ps.states.START,
-            entryPoint: cb
-        });
+            streams: {
+                stdin: new os._internals.stream.writeable(),
+                stdout: new os._internals.stream.writeable()
+            }
+        };
+
+        /*
+        the first entrypoint to get registered will pass in stdout and stdin
+         */
+        pcb.entryPoint = function () {
+            cb(pcb.streams.stdout, pcb.streams.stdin);
+        };
+
+        // for testing attaching all stdout instances to display driver
+        pcb.streams.stdout.registerDriver(os._internals.drivers.display.streamUpdateFunction);
+
+        os._internals.ps.pcb.push(pcb);
     }
 
 })();
