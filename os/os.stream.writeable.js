958736f1a9b54996383edb711617812202765076
Tomas <me@tomasre.com>
2016-03-14 22:44:59 -0700

    Added listeners for the CLI process

--- os/os.stream.writeable.js
+++ os/os.stream.writeable.js
@@ -16,6 +16,16 @@
         this._driverUpdate = null;
 
         /*
+        internal member - not to be accessed directly
+         */
+        this._streamListener = null;
+
+        /*
+        internal member - not to be accessed directly
+         */
+        this._streamListenerProcessName = '';
+
+        /*
         If the other end of the stream is to be held by a driver process
         (display device or input device)
         This will register it to be able to update synchronously
@@ -29,13 +39,33 @@
         };
 
         /*
+        A process can register a listener with a stream
+        this listener is a function which is called with one argument, a string
+        this is the input to a stream
+         */
+        this.registerStreamListener = function (listener, processName) {
+            this._streamListener = listener;
+            this._streamListenerProcessName = processName;
+        };
+
+        /*
         appends str to the buffer
         if there is a registered driver, driverUpdate will be called
+        if there is a registerStreamListener, it will be called
          */
         this.appendToBuffer = function (str) {
             this._buffer += str;
+
+            // update the display driver if one is attached
             if (this._driverUpdate) {
                 this._driverUpdate(this);
+            }
+
+            // tell the scheduler to schedule our process if it is ready
+            if (this._streamListener) {
+                os._internals.ps.fsOperationReadyToReturn(this._streamListenerProcessName, function () {
+                    this._streamListener(this);
+                });
             }
         };
 
