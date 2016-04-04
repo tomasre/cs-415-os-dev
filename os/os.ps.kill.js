0f913dd96897d7cc9c141e4d669e8c55973020b0
Edwin Young <edwin.w.young@gmail.com>
2016-04-03 18:21:30 -0700

    Refactor of kill command to use process names instead of ids

--- os/os.ps.kill.js
+++ os/os.ps.kill.js
@@ -2,13 +2,17 @@
 (function () {
     os._internals.ps.kill = killProcess;
     
-    function killProcess(pid) {
+    function killProcess(pname) {
+        pname = pname.toLowerCase();
         var i = 0;
-        while (i < os._internals.ps.pcb.length && 
-            os._internals.ps.pcb[i].id === pid) 
+        while (i < os._internals.ps.pcb.length 
+            && os._internals.ps.pcb[i].name.toLowerCase() === pname) 
             i++;
         if (i === os._internals.ps.pcb.length) 
             return { status: -1 };
+        
+        // Stop the process & return the stopped process if found
+        os._internals.ps.pcb[i].state = os._internals.ps.states.STOP;
         var removedProcess = os._internals.ps.pcb[i];
         removedProcess["status"] = 0;
         return removedProcess;
