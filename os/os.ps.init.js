30ea6f1062105f5afb7a741c5683549dbcd3ff1e
Tomas <me@tomasre.com>
2016-04-04 22:22:35 -0700

    ability to pass keyboard control from bash back to a process

--- os/os.ps.init.js
+++ os/os.ps.init.js
@@ -5,7 +5,7 @@
     os._internals.ps.copyProcessTableEntryToPCB('Bash');
     //os._internals.ps.copyProcessTableEntryToPCB('vectorCalculator');
 
-    os._internals.ps.copyProcessTableEntryToPCB('DeviceDriverTest', null, ['argument', 'argument2']);
+    //os._internals.ps.copyProcessTableEntryToPCB('DeviceDriverTest', null, ['argument', 'argument2']);
     // actually start the execution of the processes!
     os._internals.ps.scheduleProcess();
 })();
--- processes/Bash.js
+++ processes/Bash.js
@@ -37,30 +37,37 @@
                 var response = Object.getOwnPropertyNames(os._internals.fs.disk).join("<br>");
                 stdout.appendToBuffer(response);
                 break;
-            
+
             case "copy": //copy is finished
                 os._internals.ps.copyProcessTableEntryToPCB('copy', null, [command[1], command[2]]);
                 //os.ps.register('copy',os.bin.copy(command[1],command[2]));
                 stdout.appendToBuffer('Copying' +command[1]+' to destination ' + command[2]);
                 break;
-            
+
             case "rm":
                 os._internals.ps.copyProcessTableEntryToPCB('remove', null, [command[1]]);
                 stdout.appendToBuffer("removing " +command[1]);
                 break;
-            
+
             // for now hardcoded
             case "vectorcalculator":
                 os._internals.ps.copyProcessTableEntryToPCB('vectorcalculator');
                 //os.ps.register('vectorcalculator', os.bin.vectorcalculator);
                 stdout.appendToBuffer('Running VectorCalculator.js');
                 break;
-            
+
+            // for now hardcoded
+            case "drivertest":
+                os._internals.ps.copyProcessTableEntryToPCB('DeviceDriverTest');
+                //os.ps.register('vectorcalculator', os.bin.vectorcalculator);
+                stdout.appendToBuffer('Running ddrivertest.js');
+                break;
+
             case "cat":
                 os._internals.ps.copyProcessTableEntryToPCB('concatenate', null, [command[1]]);
                 //stdout.appendToBuffer(response);
                 break;
-            
+
             case "exe":
                 if(os._internals.ps.processTable[command[1]]){
                     var args = command.slice(2,command.length);
