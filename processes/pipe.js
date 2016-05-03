c9e53488b91f67a33ccd0a6313d64df5b999d024
kindbladius <mkindblad@hotmail.com>
2016-05-03 14:47:41 -0700

    Update pipe.js
    Updated changes specifications; added logic for pipeOut, including pipeOut function.  Added logic for piping buffers in both cases, so that data to be written to file or opened from file is stored in buffer instead.  Added mutex lock so piped process waiting for input waits for process piping output to complete.

--- processes/pipe.js
+++ processes/pipe.js
@@ -1,18 +1,21 @@
 /*
-	Right now ls, ps, and help can pipe into either more or cat.
-	Still need to add logic to pipe exec process outputs into either more or cat or another exec process.
-	Still need to add logic to pipe out man
+	Right now ls, ps, man, and help can pipe into more, cat, BankBookCalculator, VectorCalculator, and ContactManager
+	BankBookCalculator, VectorCalculator, and ContactManager can also pipe its output into an appropriate process
+	Still need to add logic to new processes for piping
 	Changes made:
-		Bash.js:					'ls', 'ps', and 'help' functions now have branches to determine if branch 
+		Bash.js:					'ls', 'ps', 'help' and 'man' functions now have branches to determine if branch 
 									operator found and calls the pipe function with the appropriate args
+									'exec' function now determines appropriate process, and calls pipe.js function to
+									send buffered data
 		os.ps.registerProcess.js:	CopyProcessTableEntryToPCB function now checks 'options' parameter checked in
-									to see if it is 'pipe'; if so, it's just like null, the options gets the registered
-									option object for that process, but sets the new member pipe to true (flag to tell
-									process to take input from pipe and not open the file).
+									to see if it is 'pipeIn' and/or 'pipeOut'; if so, it's just like null, the options 
+									gets the registered option object for that process, but sets the new member pipeIn/Out 
+									to true (flag to tell process to take input from pipe and not open the file, or output
+									to send to buffer instead of writing to file).
 									entryPoint function now checks pipe flag; if true, sends pipe flag to callback as true.
-		processes(more, cat):		Process registration now includes a flag member in options object, defaults to false.
-									Beginning of functions now check pipe flag in options object; if true, trickles through
-									waterfall fs ops essentially bypassing them by sending the direct string through to the 
+		processes (listed above):	Process registration now includes a pipeIn & pipeOut flag member in options object, defaults 
+									to false. Beginning of functions now check pipeIn flag in options object; if true, trickles 
+									through	waterfall fs ops essentially bypassing them by sending the direct string through to the 
 									functions that deal with the "fully read" data.  So for each fs op function, there is a 
 									branch at the beginning to check if the pipe is true, in which case it will call the next
 									waterfall function immediately with the data.
@@ -20,6 +23,8 @@
 									array based on "\n", there is now code to split it if it's "<br>" when piped in.
 									Finally, at end of these functions, there is a branch to check if pipe is true; if so, will
 									reset pipe flag in registered options; if not, will call fs close waterfall function.
+									Same with pipeOut flag and output fs ops.  At the end for closing the input, there is logic
+									to reset process pipe flags to false, as well as in error function.
 */
 
 'use strict';
